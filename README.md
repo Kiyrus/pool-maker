697748e18d392ea28b92dcd2de9d3a2feca2f084
Илья Косов <ilyakosov@MacBook-Pro-Ila.local>
2024-12-03 19:58:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Pool Maker (pool-maker)
+PollMaker (Creation) / PollTaker (Taking)
+Explanation: The first app is for creating polls, and the second one is for taking them.
+
+Application for creating and managing surveys
+Description: This application will allow users to create surveys, add questions and answers, and then share them with others. Users will be able to view the results of surveys.
+Functionality:
+Create, edit and delete surveys.
+Ability to add different types of questions (multiple choices, text answers).
+View statistics on completed surveys.
+Ability to share links to surveys via social networks or messengers.
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Lint the files
+```bash
+npm run lint
+```
+
+
+### Format the files
+```bash
+npm run format
+```
+
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="hhh lpR lFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar rounded>
+            <img src="src/assets/icon-logo.svg" alt="avatar is here">
+          </q-avatar>
+          Pool Maker
+        </q-toolbar-title>
+      </q-toolbar>
+
+<!--      <q-tabs align="left">-->
+<!--        <q-route-tab to="/page1" label="Page One" />-->
+<!--        <q-route-tab to="/page2" label="Page Two" />-->
+<!--        <q-route-tab to="/page3" label="Page Three" />-->
+<!--      </q-tabs>-->
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" behavior="desktop">
+      <span>Some content</span>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'MainLayout'
+});
+
+const leftDrawerOpen = ref(false)
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+
+</script>
